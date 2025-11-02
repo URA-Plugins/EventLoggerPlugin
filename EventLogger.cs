c3352da0561d686154f91382736f14fafee0bc9e
xulai1001 <xulai1001@gmail.com>
2025-11-02 10:04:36 +0800

    增加温泉杯适配

--- EventLogger.cs
+++ EventLogger.cs
@@ -81,9 +81,9 @@ namespace EventLoggerPlugin
     {
         public const int MinEventStrength = 25;
         // 排除佐岳充电,SS,继承,老登三选一,第三年凯旋门（输/赢）,以及无事发生直接到下一回合的情况
-        public static readonly int[] ExcludedEvents = [809043003, 400006112, 400000040, 400006474, 400006439, 830241003, -1];
+        public static readonly int[] ExcludedEvents = [809043003, 809050003, 400006112, 400000040, 400006474, 400006439, 830241003, -1];
         // 友人和团队卡不计入连续事件，这里仅排除这几个
-        public static readonly int[] ExcludedFriendCards = [30160, 30137, 30067, 30052, 10104, 30188, 10109, 30207, 30241, 30257, 10128];
+        public static readonly int[] ExcludedFriendCards = [30160, 30137, 30067, 30052, 10104, 30188, 10109, 30207, 30241, 30257, 30276, 10128, 10138];
         // 这些回合不能触发连续事件
         public static readonly int[] ExcludedTurns = [1, 25, 31, 35, 37, 38, 39, 40, 49, 51, 55, 59, 61, 62, 63, 64, 72, 73, 74, 75, 76, 77, 78];
 
@@ -105,7 +105,8 @@ namespace EventLoggerPlugin
         public static Dictionary<int, int> CardEventSpecialCount = new Dictionary<int, int>
         {
             { 30244, 1 },
-            { 30258, 1 }
+            { 30258, 1 },
+            { 30270, 1 }
         };
 
         public static LogValue LastValue;   // 前一次调用时的总属性
@@ -247,7 +248,7 @@ namespace EventLoggerPlugin
                 lastSkillTips = currentSkillTip;
 
                 var currProper = UpdateProper(@event);
-                if (lastProper.Count() == currProper.Count())
+                if (lastProper != null && lastProper.Count() == currProper.Count())
                 {
                     string[] properText = ["", "G", "F", "E", "D", "C", "B", "A", "S"];
                     foreach (var k in currProper.Keys)
